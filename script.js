3c973fff96b066ea5be31cd0d8ec7950f1ebed24
Iker Lobo Perez <ikerlobop@gmail.com>
2024-08-01 09:05:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//Hola mundo en JS
+console.log("Hola Mundo");
+
+//Utilizando variables
+
+var nombre = "Juan";
+var apellido = "Perez";
+var edad = 20;
+console.log("Hola " + nombre + " " + apellido + " tienes " + edad + " años");
+
+//comprobacion con alerts de las anteriores lineas
+alert("Hola " + nombre + " " + apellido + " tienes " + edad + " años");
+
